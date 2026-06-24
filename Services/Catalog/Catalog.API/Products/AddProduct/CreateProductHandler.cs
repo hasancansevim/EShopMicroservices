d3db195da7b8ead9662d81b55e2db45c9e0ecbd0
Hasan Can Sevim <shasancan0@gmail.com>
2026-06-24 20:05:18 +0300

    Add project files.

---
+++ Services/Catalog/Catalog.API/Products/AddProduct/CreateProductHandler.cs
@@ -0,0 +1,18 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Catalog.API.Products.AddProduct;
+
+public record CreateProductCommand(
+    string Name,List<string> Category,string Description,string ImageFile,decimal Price)
+    :ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
