f74fb8e50b84360376834f5d4906b80bfcf7bb8b
Hasan Can Sevim <shasancan0@gmail.com>
2026-07-09 01:04:15 +0300

    feat(discount): implement Discount.Grpc microservice with SQLite EF Core and gRPC endpoints

---
+++ Services/Discount/Discount.Grpc/GlobalUsing.cs
@@ -0,0 +1,6 @@
+﻿global using Grpc.Core;
+global using Mapster;
+global using Microsoft.EntityFrameworkCore;
+
+global using Discount.Grpc.Data;
+global using Discount.Grpc.Models;
---
+++ Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,77 @@
+﻿namespace Discount.Grpc.Services;
+
+public class DiscountService(DiscountContext dbContext,ILogger<DiscountService> logger)
+    :DiscountProtoService.DiscountProtoServiceBase
+{
+    public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = await dbContext
+            .Coupons
+            .FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
+
+        if (coupon is null){
+            coupon = new Coupon { ProductName = "No Discount", Amount = 0, Description = "No Discount Desc" };
+        }
+
+        logger.LogInformation(
+            "Discount is retrieved for ProductName :{ProductName}, Amount : {Amount}"
+            , coupon.ProductName, coupon.Amount
+        );
+
+        var couponModel = coupon.Adapt<CouponModel>();
+        return couponModel;
+    }
+
+    public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = request.Coupon.Adapt<Coupon>();
+
+        if (coupon is null)
+        {
+            throw new RpcException(new Status(StatusCode.InvalidArgument, "Coupon is null"));
+        }
+
+        dbContext.Add(coupon);
+        await dbContext.SaveChangesAsync();
+
+        logger.LogInformation("Discount is successfully created. ProductName : {ProductName}", coupon.ProductName);
+
+        var couponModel = coupon.Adapt<CouponModel>();
+        return couponModel;
+    }
+
+    public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = request.Coupon.Adapt<Coupon>();
+
+        if(coupon is null)
+        {
+            throw new RpcException(new Status(StatusCode.InvalidArgument, "Coupon is null"));
+        }
+
+        dbContext.Update(coupon);
+        await dbContext.SaveChangesAsync();
+
+        logger.LogInformation("Discount is successfully updated. ProductName : {ProductName}", coupon.ProductName);
+
+        var couponModel = coupon.Adapt<CouponModel>();
+        return couponModel;
+    }
+
+    public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = dbContext.Coupons.FirstOrDefault(x => x.ProductName == request.ProductName);
+
+        if (coupon is null)
+        {
+            throw new RpcException(new Status(StatusCode.NotFound, $"Discount with ProductName={request.ProductName} is not found."));
+        }
+
+        dbContext.Remove(coupon);
+        await dbContext.SaveChangesAsync();
+
+        logger.LogInformation("Discount is successfully deleted. ProductName : {ProductName}", coupon.ProductName);
+
+        return new DeleteDiscountResponse { Success = true };
+    }
+}
